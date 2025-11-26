71faa61d1667d5416e964ce6c4ed3db378d1bbf9
fluxo-solucoes-digitais <p.calisto@aluno.ifsp.edu.br>
2025-11-26 02:09:27 +0000

    configure basic php

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    
+</footer>
+
+<?php include "/includes/foot.php"; ?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include "/includes/header.php"; ?>
+
+<main class="container">
+    <div class="construction-icon">🚧</div>
+
+    <img class="construction-icon" alt="logo" src="/assets/logo.jpeg"/>
+
+    <h1>Site em Desenvolvimento</h1>
+    <p>Estamos trabalhando para trazer uma nova experiência incrível para você!</p>
+    <p>Em breve, lançaremos novas funcionalidades e melhorias.</p>
+    <p class="contact">Agradecemos sua paciência e compreensão.</p>
+</div>
+
+<?php include "/includes/footer.php"; ?>
