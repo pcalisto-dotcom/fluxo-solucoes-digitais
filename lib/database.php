be96744f422749e7dfc5c2e22fff09a6b64c4a72
DestinyFrog <pedrocalizto9@gmail.com>
2025-12-09 14:37:25 -0300

    test sqlite3

--- lib/database.php
+++ lib/database.php
@@ -1,18 +1,6 @@
 <?php
 
 function get_conn() {
-    $host = 'localhost';
-    $username = 'simplifica_fluxo_admin';
-    $password =  'M4r!pos4s';
-    $db = 'simplifica_fluxo';
-    $port = 3306;
-
-    $url = "mysql:host=$host:$port;dbname=$db";
-    $pdo = new PDO($url, $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $pdo->exec("SET NAMES 'utf8mb4'");
-    $pdo->exec("SET CHARACTER SET utf8mb4");
-    
-    return $pdo;
+    $db = new SQLite3('db.sqlite3');
+    return $db;
 }
