ddd13183c0a537d3a173f28edd77ec7929c92bcf
DestinyFrog <pedrocalizto9@gmail.com>
2025-12-09 15:09:39 -0300

    migrate sqlite3

--- database/migrate_sqlite3.php
+++ database/migrate_sqlite3.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $databaseFile = './mydb.sqlite3';
+    $databaseFile = './database/mydb.sqlite3'; 
     $db = new SQLite3($databaseFile);
 
     $init_sql = file_get_contents('./database/init.sql');
