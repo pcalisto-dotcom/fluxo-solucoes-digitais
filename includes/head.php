4d66f638b91e0010cee535565335750d9bfb00f4
Mike <mmss240610@gmail.com>
2025-12-14 19:19:25 -0300

    chore: update logo

--- includes/head.php
+++ includes/head.php
@@ -7,4 +7,5 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="/assets/style.css"/>
+    <link rel="shortcut icon" href="/assets/logo.png">
 </head>
