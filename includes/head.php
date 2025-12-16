382e7d8c02348085db70d8fb5dc4bb48510ed445
Mike <mmss240610@gmail.com>
2025-12-15 22:23:49 -0300

    fix css not rendering

--- includes/head.php
+++ includes/head.php
@@ -6,6 +6,6 @@
     <title>Fluxo Soluções Digitais</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="/assets/style.css"/>
+    <link rel="stylesheet" href="/assets/style.css?v=<?= time() ?>">
     <link rel="shortcut icon" href="/assets/logo.png">
 </head>
